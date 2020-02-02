9d723b3d03d64fc42dad89cbf9851d4021e1f351
vasu <tsvasu@gmail.com>
2020-02-02 18:27:01 -0500

    Debug

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,9 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
-}
+  // const db = config.get("db");
+  const db = process.env.vidly_db;
+  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+};
