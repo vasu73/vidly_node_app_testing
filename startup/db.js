fe07cca9b4c542c535b540273a9e051d3174d816
vasu <tsvasu@gmail.com>
2020-02-02 19:04:41 -0500

    fix

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function() {
+  
+  winston.info(`Connecting to Vasu ${db}...`));
+
   const db = config.get("db");
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
